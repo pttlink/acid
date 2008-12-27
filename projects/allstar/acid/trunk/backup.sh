8837a1c8d7dc863f25c50f285908b9c25dfd5838
steve <steve@b9360e03-a0fa-4602-804e-483810d782e8>
2008-12-27 19:18:01 +0000

    Move irlp_backup.tgz to /tmp so it is restored there.

--- projects/allstar/acid/trunk/backup.sh
+++ projects/allstar/acid/trunk/backup.sh
@@ -4,20 +4,22 @@ if [ -e /home/irlp ]
 then
 	echo "Initiating IRLP backup..."
 	(cd /home/irlp/scripts; su - repeater -c /home/irlp/scripts/backup_for_reinstall)
+	mv /home/irlp/backup/irlp_backup.tgz /tmp
 fi	
 echo "Backing up asterisk config files, and node name sound files..."
 tar cf /root/backup.tar /etc/zaptel.conf /etc/asterisk/* /var/lib/asterisk/sounds/rpt/nodenames/*
 if [ -e /home/irlp/backup/irlp_backup.tgz ]
 then
 	echo "Including irlp_backup.tgz in backup..."
-	tar rf /root/backup.tar /home/irlp/backup/irlp_backup.tgz
+	tar rf /root/backup.tar /tmp/irlp_backup.tgz
 fi
 rm -rf backup.tgz
 echo "Compressing tar archive...."
 gzip -9 backup.tar
 mv backup.tar.gz backup.tgz
-if [ -e /home/irlp/backup/irlp_backup.tgz ]
+echo "Cleaning up..."
+if [ -e /tmp/irlp_backup.tgz ]
 then
-	rm /home/irlp/backup/irlp_backup.tgz
+	rm /tmp/irlp_backup.tgz
 fi		
 echo "Done. Backup file backup.tgz is in /root" 
